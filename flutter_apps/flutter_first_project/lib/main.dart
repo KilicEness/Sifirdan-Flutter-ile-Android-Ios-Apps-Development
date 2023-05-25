5fccd7d55af1d53ae841cb56da41ac8a849c814f
enes <eneskiliicc06@gmail.com>
2023-05-25 13:44:55 +0300

    container box decoration, border, shadow

--- flutter_apps/flutter_first_project/lib/main.dart
+++ flutter_apps/flutter_first_project/lib/main.dart
@@ -4,29 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Title'),
-          backgroundColor: const Color.fromARGB(255, 28, 100, 31),
+          title: const Text('Title'),
+          backgroundColor: Colors.red,
         ),
-        body: Text(
-          'Hello World',
-          style: TextStyle(
-              color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              shape: BoxShape.rectangle,
+              border: Border.all(color: Colors.black),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(20, 20),
+                    blurRadius: 20),
+              ],
+            ),
+            child: const FlutterLogo(size: 128),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint('clicked');
           },
-          child: Icon(
+          backgroundColor: Colors.black,
+          child: const Icon(
             Icons.account_circle_rounded,
             color: Colors.red,
           ),
-          backgroundColor: Colors.black,
         ),
       ),
     );
